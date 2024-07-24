d1187e44ef7b2af76d21c1bed669ea865c19ca1a
ErenAydogan <erenaydogan@live.fr>
2024-07-24 23:45:02 +0300

    Updated

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -5,11 +5,22 @@ console.log(currentResult);
 currentResult = 0;
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 console.log(currentResult); */
+
+function add(num1, num2)
+{
+    return num1+num2
+}
+
+console.log(add(2,4))
+console.log(add(7,5))
+
+
 let defaultResult = 0;
 let currentResult = defaultResult;
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-let calculationDescription = '('+`${defaultResult}`+' + 10) * 3 / 2 - 1'
+let calculationDescription = '('+`${defaultResult}`+' + 10) * 3 / 2 - 1';
+let errorMessage = 'An error occurred';
 
 
-outputResult(currentResult, calculationDescription)
+outputResult(currentResult, errorMessage);
