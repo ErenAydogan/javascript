0d9a07e12b3d1e195400a6662c3af9ffbb7ae4ff
ErenAydogan <erenaydogan@live.fr>
2024-07-31 21:37:26 +0300

    Updated

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,11 +1,46 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add()
+const resetUserNumberInput = () =>
 {
-    currentResult = currentResult + parseInt(userInput.value);
-    userInput.value = '';
-    outputResult(currentResult, '');
+    userInput.value = null
+}
+const getUserNumberInput = () => {
+    return parseInt(userInput.value);
+}
+
+const add = () => {   
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    currentResult = currentResult + enteredNumber;
+    outputResult(currentResult, `${calcDescription}`);
+    resetUserNumberInput();
+}
+
+const subtract = () => {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} - ${enteredNumber}`;
+    currentResult = currentResult - enteredNumber;
+    outputResult(currentResult, `${calcDescription}`);
+    resetUserNumberInput();
+}
+
+const multiply = () => {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} * ${enteredNumber}`;
+    currentResult = currentResult * enteredNumber;
+    outputResult(currentResult, calcDescription);
+    resetUserNumberInput();
+}
+
+const devide = () => {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} / ${enteredNumber}`;
+    currentResult = currentResult / enteredNumber;
+    outputResult(currentResult, calcDescription)
 }
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', devide);
